dd42b07a04c98c66208f3eed56b75b19d3f15435
Alluu17 <150897351+zekoaka12vmongraal@users.noreply.github.com>
2025-11-20 10:29:28 +0200

    --

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE_URL =  "";
 
 export async function getQuestions(topic) {
   const res = await fetch(`${API_BASE_URL}/api/questions/${topic}`);
